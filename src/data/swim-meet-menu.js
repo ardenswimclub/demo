fd7f57ed577a340a8a67c5c5705b8c87fecfea7d
ardenswimclub <walter@blip.blorp>
2025-05-29 21:56:56 -0400

    navbar fixes

--- src/data/swim-meet-menu.js
+++ src/data/swim-meet-menu.js
@@ -1,13 +1,7 @@
 
 export const menu  = [
-  
-  { title: "Home", path: "/", }, 
-  { title: "Swim Team", path: "/swim-team", },     
-
-    { title: "Important Info", path: "/swim-team/swim-meets" },
-    { title: "Declare Swimmer", path: "/swim-team/swim-meets/declare-your-swimmer" },
-    { title: "Volunteer Positions", path: "/swim-team/swim-meets/volunteer-positions" },
-    { title: "Visiting Teams", path: "/swim-team/swim-meets/visiting-teams" }
-
-
+  { title: "Important Info", path: "/swim-team/swim-meets" },
+  { title: "Declare Swimmer", path: "/swim-team/swim-meets/declare-your-swimmer" },
+  { title: "Volunteer Positions", path: "/swim-team/swim-meets/volunteer-positions" },
+  { title: "Visiting Teams", path: "/swim-team/swim-meets/visiting-teams" }
 ];
--- src/data/swim-team-menu.js
+++ src/data/swim-team-menu.js
@@ -1,19 +1,8 @@
 
 export const menu  = [
-  {title: "Home",path: "/",}, 
-  
   { title: "General Info", path: "/swim-team" },  
   { title: "Registration", path: "/swim-team/registration" },
   { title: "Team Suit", path: "/swim-team/team-suit" },
   { title: "Swimmingly", path: "/swim-team/swimmingly" },
-  {
-    title: "Swim Meets",
-    path: "#",
-    children: [
-      { title: "Important Info", path: "/swim-team/swim-meets" },
-      { title: "Declare Swimmer", path: "/swim-team/swim-meets/declare-your-swimmer" },
-      { title: "Volunteer Positions", path: "/swim-team/swim-meets/volunteer-positions" },
-      { title: "Visiting Teams", path: "/swim-team/swim-meets/visiting-teams" }
-    ]
-  }
+  { title: "Swim Meets", path: "/swim-team/swim-meets" }
 ];
